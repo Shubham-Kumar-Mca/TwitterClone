7204f94bcd537571ef028ebef0e5c11a6ca0aaf9
kumarineeru211 <kumarineeru105@gmail.com>
2023-04-27 21:57:43 +0530

    Something Change

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react"
 import CreateAccount from "./pages/createaccount/CreateAccount"
+import {Routes, Route} from "react-router-dom"
+import SignIn from "./pages/signIn/SignIn"
+import Home from "./pages/home/Home"
+import PrivateRoutes from "./components/PrivateRoutes"
+import  Followers  from "./components/followers/Followers"
 function App() {
 
   return (
     <div>
-      <CreateAccount />
+    {/* <Followers/> */}
+      <Routes>
+        <Route path="/" element={<PrivateRoutes><Home /></PrivateRoutes>}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<CreateAccount />}/>
+      </Routes>
     </div>
   )
 }
