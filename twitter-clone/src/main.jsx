c4e022ac04c9f4b62b2ca24d5e6457ed23c9074e
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-01 06:10:05 +0530

    Main.jsx File

--- twitter-clone/src/main.jsx
+++ twitter-clone/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import AuthContextProvider from './context/AuthContextProvider.jsx'
+import { Provider } from "react-redux"
+import { store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
+  </Provider>
 )
