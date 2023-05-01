212ff277e757559840a661252c292247557dc59d
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-01 05:50:33 +0530

    Date Of Birth Section

--- twitter-clone/src/components/dob/DOB.jsx
+++ twitter-clone/src/components/dob/DOB.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import Select from '@mui/material/Select';
 import { InputLabel, MenuItem } from '@mui/material';
+import Select from '@mui/material/Select';
 
 const DOB = ({ handleInputChange, errors }) => {
     const years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
@@ -11,20 +11,9 @@ const DOB = ({ handleInputChange, errors }) => {
     const [day, setDay] = useState("");
     const [days, setDays] = useState([]);
 
-    // const [errors, setErrors] = useState({ day: "", month: "", year });
-    /*
-    
-    dob: {
-        day: "",
-        month: "",
-        year: "",
-    },
-    
-    */
 
     function handleMonthChange(event) {
         setMonth(event.target.value);
-        // setCompleteDate(completeData + event.target.value + ":")
     }
 
 
