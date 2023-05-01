61cc50a89ae9608a385a8a86dab4f330d947e6a0
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-01 06:07:46 +0530

    All Routes Here..

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,19 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Routes, Route } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { getTweetDataFromAPI } from "./redux/tweetData/action.tweet"
+import { getUserDataFromAPI } from "./redux/userData/action.user"
 import CreateAccount from "./pages/createaccount/CreateAccount"
-import {Routes, Route} from "react-router-dom"
+import PrivateRoutes from "./components/PrivateRoutes"
 import SignIn from "./pages/signIn/SignIn"
 import Home from "./pages/home/Home"
-import PrivateRoutes from "./components/PrivateRoutes"
-import  Followers  from "./components/followers/Followers"
+import "./App.css"
+
 function App() {
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    dispatch(getTweetDataFromAPI())
+    dispatch(getUserDataFromAPI())
+  }, [])
 
   return (
+
     <div>
-    {/* <Followers/> */}
       <Routes>
-        <Route path="/" element={<PrivateRoutes><Home /></PrivateRoutes>}/>
-        <Route path="/sign-in" element={<SignIn />}/>
-        <Route path="/sign-up" element={<CreateAccount />}/>
+        <Route path="/" element={<PrivateRoutes><Home /></PrivateRoutes>} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<CreateAccount />} />
       </Routes>
     </div>
   )
