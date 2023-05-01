25d39f9748ad8e41df82b918aaab5e7c5d1735f3
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-01 05:33:48 +0530

    Feed Section

--- twitter-clone/src/components/feed/Feed.jsx
+++ twitter-clone/src/components/feed/Feed.jsx
@@ -2,56 +2,28 @@ import React, { useContext, useEffect, useState } from 'react'
 import "./feed.css";
 import TweetBox from '../tweetBox/TweetBox';
 import Post from '../post/Post';
-// import {  useSelector } from "react-redux";
 import { AuthContext } from '../../context/AuthContextProvider';
 
 const Feed = () => {
-  // const [post, setPost] = useState(findCurrentUser.data.slice().reverse());
-  const {userData}  = useContext(AuthContext)
+  const { userData } = useContext(AuthContext)
 
 
-  // const { isLoading } = useSelector(state => state.tweet)
-  const findCurrentUser = userData.find(user=>user.isAuth);
+  const findCurrentUser = userData.find(user => user.isAuth);
   const [updatedTweets, setUpdatedTweets] = useState(findCurrentUser.data)
 
-  // useEffect(() => {
-  //   const updatedUser = userData.map(user => {
-  //     if (user.userPhone === findCurrentUser.userPhone) {
-  //       const reversedDataArray = post.slice().reverse();
-  //       return { ...user, data: reversedDataArray }
-  //     } else {
-  //       return user
-  //     }
-  //   })
-  //   setUserData(updatedUser)
-  // }, [post.length])
-
-  /* 
-
-  useEffect(() => {
-    const copyTweetedData = [...twwetsData]
-    if (usersData.length === 0) return;
-    const copyUpdated = copyTweetedData.map((user, index) => ({
-      ...user,
-      userImage: usersData[index % usersData.length].image
-    }))
-    setUpdated(copyUpdated)
-  }, [usersData])
-
-  */
 
 
   const toggleLike = (index) => {
-    const currentTweet = {...updatedTweets[index], isLiked : !updatedTweets[index].isLiked}
+    const currentTweet = { ...updatedTweets[index], isLiked: !updatedTweets[index].isLiked }
     const updatTweets = [...updatedTweets]
     currentTweet.isLiked ? currentTweet.likeCount++ : currentTweet.likeCount--;
     updatTweets[index] = currentTweet
     setUpdatedTweets(updatTweets)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     setUpdatedTweets(findCurrentUser.data)
-  },[findCurrentUser.data])
+  }, [findCurrentUser.data])
 
 
   return (
@@ -60,15 +32,13 @@ const Feed = () => {
         <h2>Home</h2>
       </div>
 
-      <TweetBox  />
+      <TweetBox />
 
-      {/* {isLoading ? <h2 style={{ textAlign: "center", marginTop: "2rem" }}>Loading...</h2> : <> */}
-        {updatedTweets &&
-          updatedTweets.map((singleUser, index) => (
-            <Post key={index} displayName={singleUser.tweetedBy?.name} emoji = {singleUser.emoji} varified={singleUser.isVarified} text={singleUser.content} avatar={singleUser.tweetedBy.userImage} image={singleUser.image} like={singleUser.likeCount} comment={singleUser.commentCount} reTweet={singleUser.reTweetsCount} isLike={singleUser.isLiked} toggleLikeValue={() => toggleLike(index)} username = {singleUser.tweetedBy.username === undefined ? singleUser.tweetedBy?.name.split(" ")[0] + Math.floor(Math.random() * 9000) + 1000 : singleUser.tweetedBy.username}/>
-          ))
-        }
-      {/* </>} */}
+      {updatedTweets &&
+        updatedTweets.map((singleUser, index) => (
+          <Post key={index} displayName={singleUser.tweetedBy?.name} emoji={singleUser.emoji} varified={singleUser.isVarified} text={singleUser.content} avatar={singleUser.tweetedBy.userImage} image={singleUser.image} like={singleUser.likeCount} comment={singleUser.commentCount} reTweet={singleUser.reTweetsCount} isLike={singleUser.isLiked} toggleLikeValue={() => toggleLike(index)} username={singleUser.tweetedBy.username === undefined ? singleUser.tweetedBy?.name.split(" ")[0] + Math.floor(Math.random() * 9000) + 1000 : singleUser.tweetedBy.username} />
+        ))
+      }
 
     </div>
   )
