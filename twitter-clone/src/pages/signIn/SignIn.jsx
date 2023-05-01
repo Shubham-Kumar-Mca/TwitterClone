b2ab31321ed6d5cf774f0bfb726f4340b77749c8
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-01 06:01:21 +0530

    SignIn Section (Made by Neeru jee)

--- twitter-clone/src/pages/signIn/SignIn.jsx
+++ twitter-clone/src/pages/signIn/SignIn.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React, { useContext, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import { BsTwitter } from "react-icons/bs"
 import { FcGoogle } from "react-icons/fc"
@@ -8,10 +7,69 @@ import { AiFillApple } from "react-icons/ai"
 
 import './signin.css'
 import { RxCross2 } from 'react-icons/rx';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContextProvider';
 
 
 export default function SignIn() {
+    const [mobileNumber, setMobileNumber] = useState("");
+    const [errors, setErrors] = useState({ userPhone: "" });
+    const navigate = useNavigate()
+    const { userData, setUserData } = useContext(AuthContext)
+
+
+    const validateMobile = () => {
+        if (!mobileNumber) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                userPhone: "Mobile number is required",
+            }));
+        } else if (!/^[0-9]+$/.test(mobileNumber)) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                userPhone: "Mobile number can only contain digits",
+            }));
+        } else if (mobileNumber.length !== 10) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                userPhone: "Mobile number should be 10 digits",
+            }));
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                userPhone: "",
+            }));
+        }
+    };
+
+    const handelValidatidate = () => {
+        validateMobile();
+        // if there are no errors, submit the form data
+        if (!errors.userPhone && mobileNumber) {
+            const filndCurrentUser = userData.find(user => user.userPhone === mobileNumber)
+            // const updatedUser = {...filndCurrentUser, isAuth : true}
+            if (!filndCurrentUser) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    userPhone: "Mobile number do not match. Please sign up first!",
+                }));
+                navigate("/sign-up");
+            } else {
+                const updatedUser = userData.map(user => {
+                    if (user.userPhone === filndCurrentUser.userPhone) {
+                        return { ...user, isAuth: true }
+                    } else {
+                        return user
+                    }
+                })
+                setUserData(updatedUser)
+                navigate("/")
+                setMobileNumber("")
+            }
+        }
+    }
+
+
     return (
         <section className='signIn__container'>
             <div className='singIn__wrapper'>
@@ -45,18 +103,21 @@ export default function SignIn() {
                     </div>
 
                     <TextField
-                        label="Email"
+                        label="Mobile Number"
                         variant="outlined"
                         margin="normal"
                         required
-                        fullWidth 
-                        type="email"
+                        fullWidth
+                        type="text"
                         className="email-field"
+                        value={mobileNumber}
+                        onChange={(e) => setMobileNumber(e.target.value)}
                     />
-                    
-                    <button className='btn__next'>Next</button>
-                    
-                    <button className='btnpassword'>
+                    {errors.userPhone && <label className='error mt-1'>{errors.userPhone}</label>}
+
+                    <button className='btn__next' onClick={handelValidatidate}>Next</button>
+
+                    <button className='btnpassword' >
                         <span>Forgot password?</span>
                     </button>
 
