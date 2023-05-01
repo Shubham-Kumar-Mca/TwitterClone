6f184fefbe85c3976dace19ad5ce268f3a4fc95b
kumarineeru211 <kumarineeru105@gmail.com>
2023-05-01 15:00:02 +0530

    search updated

--- twitter-clone/src/components/search/Search.jsx
+++ twitter-clone/src/components/search/Search.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import {FaSistrix} from 'react-icons/fa'
+import { FaSistrix } from 'react-icons/fa'
 import './search.css'
 
 function Search() {
   return (
-    <>
     <div className='container_search'>
-     
-        <FaSistrix className='icon_search' />
-        <input   type='text' placeholder='Search Twitter' className='Input_search'
-            onChange={(e)=>e.target.value}
-        />
-      </div>
-      </>
+      <input type='text' placeholder='Search Twitter' className='Input_search'
+        onChange={(e) => e.target.value}
+      />
+      <FaSistrix className='icon_search' />
+
+    </div>
   )
 }
 
