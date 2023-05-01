f3098d20cc3b3e7ee04d9aef1af058098098683b
mondalnilavra13 <mondalnilavra13@gmail.com>
2023-05-02 01:10:27 +0530

    Right side section

--- twitter-clone/src/pages/home/Home.jsx
+++ twitter-clone/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Followers from '../../components/followers/Followers'
 import Search from '../../components/search/Search'
 import Feed from '../../components/feed/Feed'
 import "./home.css"
+import { Rightside1 } from '../../components/Rightpart/Rightside1'
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       {/* Right Section */}
       <div className='right__section'>
         <Search />
+        <Rightside1 />
         <Followers />
       </div>
     </section>
