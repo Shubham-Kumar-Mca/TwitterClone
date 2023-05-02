fbb21b807c0bd1d6d5ebc394fbe71706db83e6a1
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-03 01:15:57 +0530

    Home Page Updated

--- twitter-clone/src/pages/home/Home.jsx
+++ twitter-clone/src/pages/home/Home.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Followers from '../../components/followers/Followers'
 import Search from '../../components/search/Search'
-import Feed from '../../components/feed/Feed'
+import Feed from '../../components/feed/Feed';
 import "./home.css"
-import { Rightside1 } from '../../components/Rightpart/Rightside1'
+import { Rightside1 } from '../../components/Rightpart/Rightside1';
 
 const Home = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollTop(window.pageYOffset);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <section className='home__container'>
       {/* sidebar */}
       <Sidebar />
 
-      {/* Feed */}
-      <Feed />
+      <div className='both__fedd__and__right'>
+        {/* Feed */}
+        <Feed />
 
-
-      {/* Right Section */}
-      <div className='right__section'>
-        <Search />
-        <Rightside1 />
-        <Followers />
+        {/* Right Section */}
+        <div className='right__section'>
+          <div className={`search__section ${scrollTop > 200 ? 'fixed-se' : ''}`} >
+            <Search />
+          </div>
+          <Rightside1 />
+          <div className={`followers__section ${scrollTop > 200 ? 'fixed' : ''}`}>
+            <Followers />
+          </div>
+        </div>
       </div>
     </section>
   )
